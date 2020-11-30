a4136f186497d2acf7b56b7bbb2a8fccc7d5ef25
Ingvar <Dimon_chick@mail.ru>
2020-11-30 21:25:48 +0900

    2.5 the job done

--- lesson 2.5.py
+++ lesson 2.5.py
@@ -1,7 +1,12 @@
 rating = [7, 5, 3, 3, 2]
-n = 0
-number = int(input('Введите число: '))
-while number < rating[n]:
-    n = n+1
-else: rating.insert(n, number)
-print(rating)
+for i in range(int(input('Сколько чисел будете вводить?  '))):
+    n = 0
+    number = int(input('Введите число: '))
+    while number < rating[n]:
+        if n < len(rating)-1:
+            n = n+1
+        elif n >= len(rating)-1:
+            rating.insert(n+1, number)
+            break
+    else: rating.insert(n, number)
+    print(rating)
